772f2776f0905c62249bde85c7322bbcbb4f4adb
lukkspereiraa <107278072+lukkspereiraa@users.noreply.github.com>
2024-11-26 14:41:52 -0300

    chore: modifying the logic of selcation sort add  max_index

---  classes/selectionSort.py
+++  classes/selectionSort.py
@@ -1,14 +1,20 @@
 def selectionSort(array, size):
     
-    for ind in range(size-1):
+    for ind in range(size - 1):
         min_index = ind
+        max_index = size-1
  
         for j in range(ind + 1, size):
-           
+            
+            if array[j] > array[max_index]:
+                max_index = j
+
             if array[j] < array[min_index]:
                 min_index = j
-     
+        
+        (array[max_index], array[ind]) = (array[max_index], array[ind])
         (array[ind], array[min_index]) = (array[min_index], array[ind])
+        
  
 arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
 size = len(arr)
