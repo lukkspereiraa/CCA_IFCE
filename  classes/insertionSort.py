05676ae45936c4f70673599b499c8ca644430e4e
lukkspereiraa <107278072+lukkspereiraa@users.noreply.github.com>
2024-12-02 16:07:48 -0300

    chore: indentado codigo

---  classes/insertionSort.py
+++  classes/insertionSort.py
@@ -7,9 +7,11 @@ def insertionSort(arr):
     for i in range(1, n): 
         key = arr[i]  
         j = i-1
+        
         while j >= 0 and key < arr[j]: 
             arr[j+1] = arr[j]  
             j -= 1
+
         arr[j+1] = key 
   
 
